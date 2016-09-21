87aa6950578933b17be7f426d4326060fffc1621
Organatik <organatik@gmail.com>
2016-09-21 15:53:34 +0300

    push

---
+++ js/common.js
@@ -0,0 +1,30 @@
+$(function() {
+	// menu
+    var combinedMenu = $('#menu2');
+    combinedMenu.slicknav({
+        label: '',
+        duration: 100,
+        prependTo:'.adaptive_menu',
+        closeOnClick: true,
+        parentTag : 'a',
+
+    });
+    //BLUR
+    $('.slicknav_btn').click(function () {
+        $('#blur').toggleClass('blur','addOrRemove')
+
+    });
+    $('.slicknav_item, .slicknav_open').click(function () {
+        $(this).toggleClass('open_menu','addOrRemove');
+        $('.arrow_l',this).toggleClass('arr_l', 'addOrRemove');
+    });
+    $('.sprache_down').click(function () {
+        $('.slicknav_menu .SPR').toggleClass('on','addOrRemove');
+
+    })
+    $('.footer_list').click(function () {
+        $('.footer_drop',this).toggleClass('footer_drop_on','addOrRemove');
+        $('.fa-caret-down',this).toggleClass('fa-caret-up ','addOrRemove');
+    })
+
+});
